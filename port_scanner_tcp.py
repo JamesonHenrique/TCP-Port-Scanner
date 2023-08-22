cb37dd37e68c99d1016a80bfc22a6117ce50c6b2
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2023-08-21 21:59:09 -0300

    Altereçoes

--- port_scanner_tcp.py
+++ port_scanner_tcp.py
@@ -1,13 +1,18 @@
 import socket
-import sys
 ip = input("Digite o IP:")
 ports = range(1, 1025)
+open_ports = False
+print("Procurando portas...\n")
 for port in ports:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.settimeout(0.06)
+    client.settimeout(0.09)
     code = client.connect_ex((ip, port))
-    
     if code == 0:
         print(str(port) + " - Porta aberta", )
-    client.close()
+        open_ports = True
+        client.close()
+
+if not open_ports:
+    print("Portas abertas não encotradas")
+ 
 print("Scan finalizada")
