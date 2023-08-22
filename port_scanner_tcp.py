32ac5191e328de31fff45514c64342e99e8d5d79
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2023-08-22 19:50:32 -0300

    Coloquei try e except para ips invalidos
    Fiz uma mudança para que se for digitado um ip invalido apareça

--- port_scanner_tcp.py
+++ port_scanner_tcp.py
@@ -3,16 +3,20 @@ ip = input("Digite o IP:")
 ports = range(1, 1025)
 open_ports = False
 print("Procurando portas...\n")
-for port in ports:
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.settimeout(0.09)
-    code = client.connect_ex((ip, port))
-    if code == 0:
-        print(str(port) + " - Porta aberta", )
-        open_ports = True
-        client.close()
+try:
+    for port in ports:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.settimeout(0.09)
+        code = client.connect_ex((ip, port))
+        if code == 0:
+            print(str(port) + " - Porta aberta", )
+            open_ports = True
+            client.close()
 
-if not open_ports:
-    print("Portas abertas não encotradas")
+
+    if not open_ports:
+        print("Portas abertas não encotradas")
+except:
+    print("Endereço de IP invalido.")
  
-print("Scan finalizada")
+    print("Scan finalizada")
