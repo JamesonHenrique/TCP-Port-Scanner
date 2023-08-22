ce7fa12cd777154bc6d9d79056613d992903c4f0
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2023-08-21 21:31:53 -0300

    Alteraçoes
    Resolvendo testes

--- port_scanner_tcp.py
+++ port_scanner_tcp.py
@@ -1,23 +1,13 @@
-
-
 import socket
-
-# Solicita ao usuário para inserir o endereço IP alvo.
+import sys
 ip = input("Digite o IP:")
-
-# Cria uma lista de números de porta de 0 a 1023.
-ports = range(1024)
-
-# Loop para cada número de porta na lista.
+ports = range(1, 1025)
 for port in ports:
-    # Cria um soquete TCP e define um tempo limite de 0.05 segundos.
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.settimeout(0.05)
-    
-    # Tenta conectar ao IP e porta atual.
+    client.settimeout(0.06)
     code = client.connect_ex((ip, port))
     
-    # Se a conexão tiver êxito (código 0), exibe "Porta aberta" com o número da porta.
     if code == 0:
-        print(str(port) + " - Porta aberta")
+        print(str(port) + " - Porta aberta", )
+    client.close()
 print("Scan finalizada")
